b4f0490f027d25a173a6b756a254453b6a7c30e5
Leopold <js2spine@gmail.com>
2025-03-16 11:33:03 +0200

    font-log

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,7 +3,3 @@
 import { Inter , Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = lusitana({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-});
